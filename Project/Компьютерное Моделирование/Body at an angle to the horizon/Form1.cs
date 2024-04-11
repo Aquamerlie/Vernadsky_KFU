cf79d1a862ebb20ba875982a4bc5f1db308801fd
Aquamerlie <76911270+Aquamerlie@users.noreply.github.com>
2024-04-11 08:42:03 +0300

    Add files via upload

---
+++ Project/Компьютерное Моделирование/Body at an angle to the horizon/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modelirovanie
+{
+    public partial class Form1 : Form
+    {
+
+        const int array = 10000;
+        double v0, fi, windx;
+             
+        double[] x = new double[array];
+        double[] y = new double[array];
+        double[] t = new double[array];
+        double[] vmod = new double[array];
+
+        public vect a = new vect();
+        public vect v = new vect();
+        public vect wind = new vect();
+        public vect r = new vect(0, 0, 0);
+        public vect g = new vect(0, -10, 0);
+
+        int iter = 0;
+        double dt = 0.1;
+        double vmax = 10;
+        
+
+        public struct vect
+        {
+            public double x, y, z;
+            public double modul() => Math.Sqrt(x * x + y * y + z * z);            
+            public double Mod { get { return Math.Sqrt(x * x + y * y + z * z); } }     
+            public vect(double x, double y, double z) { this.x = x; this.y = y; this.z = z; }
+            public static vect operator +(vect a, vect b) => new vect(a.x + b.x, a.y + b.y, a.z + b.z);
+            public static double operator *(vect a, vect b) => a.x * b.x + a.y * b.y; 
+            public static vect operator *(double c, vect a) => new vect(a.x * c, a.y * c, a.z * c);
+            public static vect operator *(vect a, double c) => new vect(a.x * c, a.y * c, a.z * c);
+            public static vect operator /(vect a, double c) => new vect(a.x / c, a.y / c, a.z / c);
+            public static vect operator -(vect a, vect b) => new vect(a.x - b.x, a.y - b.y, a.z - b.z);
+            public static vect operator &(vect a, vect b) => new vect(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            buttonStart.Enabled = true;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            while (vmax > 5)
+            {
+                vmax /= 2;
+                if (vmax < 5)
+                {
+                    vmax = 10;
+                }
+            }
+        }
+        // Физик
+        public vect accel(vect g, vect v, double vmax, vect wind, double h)
+        {
+            h = 0;
+            return g - Math.Exp(-h / 10000) * g.Mod * (v - wind) * (v - wind).Mod / (vmax * vmax);
+        }
+
+        // Математик
+        private void AlgoritmVerle(double dt, vect g, ref vect r, ref vect v, double vmax, vect wind)
+        {           
+            vect a = new vect();
+            vect vPred = new vect();
+            a = accel(g, v, vmax, wind, r.y);
+
+            r += v * dt + 0.5 * a * dt * dt;
+            vPred = v + a * dt;
+            v += 0.5 * a * dt;
+            a = accel(g, vPred, vmax, wind, r.y);
+            v += 0.5 * a * dt;
+
+        }
+        //Художник - программист
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            iter++;
+            AlgoritmVerle(dt, g, ref r, ref v, vmax, wind);
+
+            if ((iter >= array) || (r.y < 0))
+            {
+                timer1.Stop();
+                return;
+            }
+
+            y[iter] = r.y;
+            x[iter] = r.x;
+            vmod[iter] = v.Mod;
+            t[iter] = t[iter] + dt;
+            chartTr.Series[0].Points.Clear();
+            chartVt.Series[0].Points.Clear();
+
+            for (int i = 0; i <= iter; i++)
+            {
+                chartTr.Series[0].Points.AddXY(x[i], y[i]);
+                chartVt.Series[0].Points.AddXY(t[i], vmod[i]);
+            }
+
+        }
+
+       private void buttonStart_Click(object sender, EventArgs e)
+        {
+            r.x = 0;
+            r.y = 0;
+            x[0] = 0;
+            y[0] = 0;
+            iter = 0;
+            vmod[0] = 0;
+            t[0] = 0;
+
+            buttonStart.Enabled = false;            
+            timer1.Start();
+
+            if (!(Double.TryParse(textBoxdt.Text, out dt)))
+            {
+                dt = 0.1;
+                textBoxdt.Text = dt.ToString();
+            }
+
+            if (!(Double.TryParse(textBoxFi.Text, out fi)))
+            {
+                MessageBox.Show("Угол");
+                return;
+            }
+            fi *= Math.PI / 180;
+
+            if (!(Double.TryParse(textBoxV0.Text, out v0)))
+            {
+                MessageBox.Show("Начальная Cкорость");
+                return;
+            }
+            v.x = v0 * Math.Cos(fi);
+            v.y = v0 * Math.Sin(fi);
+
+            if(!(Double.TryParse(textBoxVmax.Text, out vmax)))
+            {
+                MessageBox.Show("Установившееся сокрость");
+                return;
+            }
+
+            if (!(Double.TryParse(textBoxWind.Text, out windx)))
+            {
+                MessageBox.Show("Ветер");
+            }
+            wind.x = windx;
+
+        }
+    }
+}
---
+++ Project/Компьютерное Моделирование/John Conway's Cellular Automaton/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace Kolonisazia_Marsa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.butStop = new System.Windows.Forms.Button();
+            this.butStart = new System.Windows.Forms.Button();
+            this.labelDensity = new System.Windows.Forms.Label();
+            this.numDistance = new System.Windows.Forms.NumericUpDown();
+            this.labelResolution = new System.Windows.Forms.Label();
+            this.numApproach = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDistance)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numApproach)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.splitContainer1.Panel2.Controls.Add(this.butStop);
+            this.splitContainer1.Panel2.Controls.Add(this.butStart);
+            this.splitContainer1.Panel2.Controls.Add(this.labelDensity);
+            this.splitContainer1.Panel2.Controls.Add(this.numDistance);
+            this.splitContainer1.Panel2.Controls.Add(this.labelResolution);
+            this.splitContainer1.Panel2.Controls.Add(this.numApproach);
+            this.splitContainer1.Size = new System.Drawing.Size(1186, 611);
+            this.splitContainer1.SplitterDistance = 996;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(992, 607);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // butStop
+            // 
+            this.butStop.BackColor = System.Drawing.Color.MediumPurple;
+            this.butStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.butStop.Location = new System.Drawing.Point(20, 194);
+            this.butStop.Name = "butStop";
+            this.butStop.Size = new System.Drawing.Size(116, 35);
+            this.butStop.TabIndex = 5;
+            this.butStop.Text = "Stop";
+            this.butStop.UseVisualStyleBackColor = false;
+            this.butStop.Click += new System.EventHandler(this.butStop_Click);
+            // 
+            // butStart
+            // 
+            this.butStart.BackColor = System.Drawing.Color.MediumPurple;
+            this.butStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.butStart.Location = new System.Drawing.Point(20, 136);
+            this.butStart.Name = "butStart";
+            this.butStart.Size = new System.Drawing.Size(116, 35);
+            this.butStart.TabIndex = 4;
+            this.butStart.Text = "Start";
+            this.butStart.UseVisualStyleBackColor = false;
+            this.butStart.Click += new System.EventHandler(this.butStart_Click);
+            // 
+            // labelDensity
+            // 
+            this.labelDensity.AutoSize = true;
+            this.labelDensity.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelDensity.Location = new System.Drawing.Point(13, 70);
+            this.labelDensity.Name = "labelDensity";
+            this.labelDensity.Size = new System.Drawing.Size(87, 16);
+            this.labelDensity.TabIndex = 3;
+            this.labelDensity.Text = "Дистанция";
+            // 
+            // numDistance
+            // 
+            this.numDistance.Location = new System.Drawing.Point(16, 87);
+            this.numDistance.Name = "numDistance";
+            this.numDistance.Size = new System.Drawing.Size(120, 20);
+            this.numDistance.TabIndex = 2;
+            this.numDistance.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numDistance.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // labelResolution
+            // 
+            this.labelResolution.AutoSize = true;
+            this.labelResolution.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelResolution.Location = new System.Drawing.Point(13, 19);
+            this.labelResolution.Name = "labelResolution";
+            this.labelResolution.Size = new System.Drawing.Size(120, 16);
+            this.labelResolution.TabIndex = 1;
+            this.labelResolution.Text = "Примближение";
+            // 
+            // numApproach
+            // 
+            this.numApproach.Location = new System.Drawing.Point(16, 36);
+            this.numApproach.Name = "numApproach";
+            this.numApproach.Size = new System.Drawing.Size(120, 20);
+            this.numApproach.TabIndex = 0;
+            this.numApproach.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numApproach.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1186, 611);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDistance)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numApproach)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button butStop;
+        private System.Windows.Forms.Button butStart;
+        private System.Windows.Forms.Label labelDensity;
+        private System.Windows.Forms.NumericUpDown numDistance;
+        private System.Windows.Forms.Label labelResolution;
+        private System.Windows.Forms.NumericUpDown numApproach;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
